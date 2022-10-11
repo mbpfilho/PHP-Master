6d899231e979e3a099db258bb06b7ddb7753f94f
Marcio Bueno <mabuen@live.com>
2022-10-11 12:38:18 +0200

    aula 24

---
+++ aula24/paracasa.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>para casa</title>
+</head>
+<body>
+    <?php
+    $anos=45;
+    $meses=10;
+    $dias=22;
+
+    $idade=($anos*365)+($meses*30)+$dias;
+
+    echo "A idade em dias é: ".$idade;
+    ?>
+</body>
+</html>
